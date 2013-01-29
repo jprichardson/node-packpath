2c3749ba442688df33d8029bd5548dfef57666e8
JP Richardson <jprichardson@gmail.com>
2013-01-29 09:49:15 -0600

    Cleaned up test file.

--- test/packpath.test.js
+++ test/packpath.test.js
@@ -1,23 +1,21 @@
-var assert = require('assert');
-var faux_module_parent = require('faux-module-parent');
+var testutil = require('testutil')
+  , faux_module_parent = require('faux-module-parent');
 
-
-var T = function(expr) { assert(expr); }
 
 describe('packpath', function(){
-    describe('+ self()', function(){
-        it('should return the directory of the package.json for faux-module-self', function(){ 
-            //console.log(faux_module_parent.self());
-            //console.log(faux_module_parent.selfFromPackPath());
-            T(faux_module_parent.self() === faux_module_parent.selfFromPackPath());
-        });
-    });
+  describe('+ self()', function(){
+    it('should return the directory of the package.json for faux-module-self', function(){ 
+      //console.log(faux_module_parent.self());
+      //console.log(faux_module_parent.selfFromPackPath());
+      EQ (faux_module_parent.self(), faux_module_parent.selfFromPackPath())
+    })
+  })
 
-    describe('+ parent()', function(){
-        it('should return the directory of the package.json for faux-module-parent', function(){ 
-            //console.log(faux_module_parent.parent());
-            //console.log(faux_module_parent.parentFromPackPath());
-            T(faux_module_parent.parent() === faux_module_parent.parentFromPackPath());
-        });
-    });
-});
+  describe('+ parent()', function(){
+    it('should return the directory of the package.json for faux-module-parent', function(){ 
+      //console.log(faux_module_parent.parent());
+      //console.log(faux_module_parent.parentFromPackPath());
+      EQ (faux_module_parent.parent(), faux_module_parent.parentFromPackPath())
+    })
+  })
+})
